210d624f46052c90a5de5a1d56fdae1d366712d5
Mobius K <mail@mobiusk.com>
2020-04-11 23:41:47 -0400

    New service worker for offline usage

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,14 +1,16 @@
 import { environment } from "portfolio/scripts/environment";
 import { ErrorMonitor } from "shared/scripts/error-monitor";
 
-// Start the error watcher for the production environment
 if (environment.production) {
-	new ErrorMonitor("https://0e3157e4be3249678b85fd9d879b9538@sentry.io/5178458");
-}
 
-// Load our service worker to enable PWA functionality
-if ("serviceWorker" in navigator) {
-	window.addEventListener("load", () => {
-		navigator.serviceWorker.register("service-worker.js").then();
-	});
+	// Start the error watcher
+	new ErrorMonitor("https://0e3157e4be3249678b85fd9d879b9538@sentry.io/5178458");
+
+	// Load our service worker to enable PWA functionality
+	if ("serviceWorker" in navigator) {
+		window.addEventListener("load", () => {
+			navigator.serviceWorker.register("/service-worker.js").then();
+		});
+	}
+
 }
---
+++ scripts/service-worker.ts
@@ -0,0 +1,36 @@
+import { ServiceWorkerCache } from "shared/scripts/service-worker-cache";
+
+// Identifier used to ensure browsers are caching the right version of assets
+const cacheId = "2";
+
+// Cache manipulator
+const cache: ServiceWorkerCache = new ServiceWorkerCache();
+
+// @ts-ignore Install the service worker and cache any files for offline use
+self.addEventListener("install", (event: InstallEvent) => {
+
+	event.waitUntil(cache.add(cacheId, [
+		"/404.html",
+		"/favicon.ico",
+		"/icon.png",
+		"/index.html",
+		"/manifest.json",
+		"/offline.html",
+		"/portfolio.css",
+		"/portfolio.js",
+		"/robots.txt",
+	]));
+
+	// @ts-ignore Don't require browsers to close and re-open tab
+	self.skipWaiting();
+});
+
+// @ts-ignore Activate the service worker and remove previous caches
+self.addEventListener("activate", (event: ExtendableEvent) =>
+	event.waitUntil(cache.deleteCaches((id: string) => id != cacheId))
+);
+
+// @ts-ignore Get requests from cache, network, or a fallback
+self.addEventListener("fetch", (event: FetchEvent) =>
+	event.respondWith(cache.fetchWithCacheOnNetwork(cacheId, event.request, "/offline.html"))
+);
