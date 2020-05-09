d474ccdad911a912b27222cd412e34520b517505
Mobius K <mail@mobiusk.com>
2020-05-09 16:32:19 -0400

    Turning off error monitor for time being
    There is no script to monitor except service worker.
    Sentry is far too heavy to justify for that.

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,10 +1,11 @@
 import { environment } from "portfolio/scripts/environment";
-import { ErrorMonitor } from "shared/scripts/error-monitor";
+// import { ErrorMonitor } from "shared/scripts/error-monitor";
 
 if (environment.production) {
 
 	// Start the error watcher
-	new ErrorMonitor("https://0e3157e4be3249678b85fd9d879b9538@sentry.io/5178458");
+	// This only needs to be enabled when significant scripting is added
+	// new ErrorMonitor("https://0e3157e4be3249678b85fd9d879b9538@sentry.io/5178458");
 
 	// Load our service worker to enable PWA functionality
 	if ("serviceWorker" in navigator) {
