007dae277b76347b1bb30d6e706ed6f150e9f127
Mobius K <mail@mobiusk.com>
2020-08-14 19:20:14 -0400

    Adjusting Sentry Raven client to be loaded from CDN with new DSN

--- scripts/index.ts
+++ scripts/index.ts
@@ -4,8 +4,8 @@ import { environment } from "portfolio/scripts/environment";
 if (environment.production) {
 
 	// Start the error watcher
-	// This only needs to be enabled when significant scripting is added
-	// new ErrorMonitor("https://0e3157e4be3249678b85fd9d879b9538@sentry.io/5178458");
+	// This only needs to be enabled when significant scripting is added (don't forget to include the CDN script tag)
+	// new ErrorMonitor("https://0e3157e4be3249678b85fd9d879b9538@o127434.ingest.sentry.io/5178458");
 
 	// Load our service worker to enable PWA functionality
 	if ("serviceWorker" in navigator) {
