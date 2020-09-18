700d2db53506fa3778c14e17939a9bd78b79fea1
Mobius K <mail@mobiusk.com>
2020-09-17 22:02:29 -0400

    Potential persistent service worker cache update

--- scripts/service-worker.ts
+++ scripts/service-worker.ts
@@ -1,12 +1,13 @@
 import { ServiceWorkerCache } from "shared/scripts/service-worker-cache";
 
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
 // Cache manipulator
-const cache: ServiceWorkerCache = new ServiceWorkerCache();
+const cache: ServiceWorkerCache = new ServiceWorkerCache("replaceAtBuildWithDate");
 
 // @ts-ignore Install the service worker and cache any files for offline use
-self.addEventListener("install", (event: InstallEvent) => {
-
-	event.waitUntil(cache.add([
+self.addEventListener("install", (event: InstallEvent) => event.waitUntil(
+	cache.initialize([
 		"/404.html",
 		"/favicon.ico",
 		"/icon.png",
@@ -16,22 +17,25 @@ self.addEventListener("install", (event: InstallEvent) => {
 		"/portfolio.css",
 		"/portfolio.js",
 		"/robots.txt",
-	]));
-
-	// @ts-ignore Don't require browsers to close and re-open tab
-	self.skipWaiting();
-});
+	]).then(() =>
+		// @ts-ignore New service workers require a tab close and re-open to activate without this
+		self.skipWaiting()
+	)
+));
 
 // @ts-ignore Activate the service worker and remove previous caches
 self.addEventListener("activate", (event: ExtendableEvent) => event.waitUntil(
-	cache.deletePreviousCaching()
+	cache.deletePreviousCaching().then(() =>
+		// @ts-ignore New service workers can immediately take control over current service workers
+		self.clients.claim()
+	)
 ));
 
-// @ts-ignore Response priority: Cache > Network (cache after) > Offline message
+// @ts-ignore Response priority: Cache > Network > Offline message
 self.addEventListener("fetch", (event: FetchEvent) => event.respondWith(
 	cache.fetchOrFallback(
 		event.request,
-		async (request: Request) => cache.put(request, await fetch(request)),
+		() => fetch(event.request),
 		() => cache.match("/offline.html")
 	)
 ));
