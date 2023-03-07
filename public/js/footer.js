73e4a95d0848bdc54d9aaad04b20bc3e752f0af9
Mobius K <mail@mobiusk.com>
2023-03-06 22:32:13 -0500

    Adding Github and Twitter links to footer

--- public/js/footer.js
+++ public/js/footer.js
@@ -7,7 +7,11 @@ class Footer extends HTMLElement {
 			<footer>
 				<a href="mailto:mail@mobiusk.com">Email</a>
 				<br>
+				<a href="https://mobiusk.com/git">Github</a>
+				<br>
 				<a href="feed.xml">RSS</a>
+				<br>
+				<a href="https://mobiusk.com/twitter">Twitter</a>
 			</footer>
 		`;
 	}
