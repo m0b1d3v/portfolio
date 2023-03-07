f302c67c2353dc93b3c5cc1ff6b3d0e5ed3deb1c
Mobius K <mail@mobiusk.com>
2023-03-06 22:33:38 -0500

    Removing all center-aligned text in favor of left-aligned text

--- public/js/header.js
+++ public/js/header.js
@@ -3,8 +3,6 @@ class Header extends HTMLElement {
 	connectedCallback() {
 		this.innerHTML = `
 			<img src="logo.png" alt="Brand logo" width="320px" height="119px">
-
-			<h1>MobiusK</h1>
 			<hr>
 		`;
 	}
