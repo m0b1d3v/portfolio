ad78c3917a258c826ee50b691fd2014e7f8cecc9
Mobius K <mail@mobiusk.com>
2023-03-17 00:52:35 -0400

    Forcing Playwright browser mobile sizes

--- playwright.config.js
+++ playwright.config.js
@@ -27,11 +27,14 @@ module.exports = defineConfig({
 
 	// Configure projects for major browsers
 	projects: [
-		{ name: 'chromium', use: {...devices['Desktop Chrome']}},
-		{ name: 'firefox', use: {...devices['Desktop Firefox']}},
-		{ name: 'webkit', use: {...devices['Desktop Safari']}},
-		{ name: 'Mobile Chrome', use: {...devices['Pixel 5']}},
-		{ name: 'Mobile Safari', use: {...devices['iPhone 12']}},
+
+		{ name: 'chromium', use: {browserName: 'chromium'}},
+		{ name: 'chromium-mobile', use: {browserName: 'chromium', viewport: { width: 360, height: 640 }}},
+
+		{ name: 'firefox', use: {browserName: 'firefox'}},
+
+		{ name: 'webkit', use: {browserName: 'webkit'}},
+		{ name: 'webkit-mobile', use: {browserName: 'webkit', viewport: { width: 360, height: 640 }}},
 	],
 
 	// Folder for test artifacts such as screenshots, videos, traces, etc.
