4f2f7e5145bde497ed9ea3da221a36c0e32bdeb6
Mobius K <mail@mobiusk.com>
2023-07-20 00:29:47 -0400

    Testing screenshots for all pages

--- src/test/java/com/mobiusk/portfolio/E2ETests.java
+++ src/test/java/com/mobiusk/portfolio/E2ETests.java
@@ -3,10 +3,14 @@ package com.mobiusk.portfolio;
 import com.deque.html.axecore.playwright.AxeBuilder;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.ScreenshotScale;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class E2ETests extends TestBase {
@@ -50,6 +54,24 @@ class E2ETests extends TestBase {
 
 	@ParameterizedTest
 	@EnumSource(PageEnum.class)
+	void screenshotDesktop(PageEnum pageEnum) {
+
+		navigate(pageEnum);
+
+		var screenshotFileName = formatPageLinkForFileName(pageEnum);
+		var destination = String.format("build/test-results/screenshots/%s.png", screenshotFileName);
+
+		var screenshotOptions = new Page.ScreenshotOptions();
+		screenshotOptions.scale = ScreenshotScale.CSS;
+		screenshotOptions.setPath(Paths.get(destination));
+
+		var screenshotBytes = page.screenshot(screenshotOptions);
+
+		assertNotNull(screenshotBytes);
+	}
+
+	@ParameterizedTest
+	@EnumSource(PageEnum.class)
 	void title(PageEnum pageEnum) {
 
 		navigate(pageEnum);
@@ -58,8 +80,22 @@ class E2ETests extends TestBase {
 	}
 
 	// Test utility method(s)
+
 	private void navigate(PageEnum pageEnum) {
 		page.navigate(pageEnum.getLink());
+	}
+
+	private String formatPageLinkForFileName(PageEnum pageEnum) {
+
+		var fileName = pageEnum.getLink();
+		fileName = fileName.replaceAll("/", "-");
+		fileName = fileName.substring(1); // All links should start with '/'
+
+		if (fileName.isEmpty()) {
+			fileName = "home";
+		}
+
+		return fileName;
 	}
 
 }
