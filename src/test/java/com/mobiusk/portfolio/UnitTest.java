9ad89f8036b5da02d08040d22b639ad65ac5604f
Mobius K <mail@mobiusk.com>
2023-07-19 22:10:42 -0400

    Setting up JUnit for testing

---
+++ src/test/java/com/mobiusk/portfolio/UnitTest.java
@@ -0,0 +1,14 @@
+package com.mobiusk.portfolio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UnitTest {
+
+	@Test
+	void addition() {
+		assertEquals(4, 2+2);
+	}
+
+}
